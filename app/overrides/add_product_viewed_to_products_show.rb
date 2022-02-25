6d10a78d69709bd1f1b655e5eafbd1ff7ebb0011
Sky <befruitful12@gmail.com>
2022-02-25 09:49:38 -0500

    add original to ga product view

--- app/overrides/add_product_viewed_to_products_show.rb
+++ app/overrides/add_product_viewed_to_products_show.rb
@@ -11,5 +11,6 @@ Deface::Override.new(
     name: 'add_ga_product_viewed_to_products_show',
     insert_bottom: '[data-hook="product_show"]',
     partial: 'spree/shared/trackers/google_analytics/product_viewed.js'
+    original: 'a890bf88c4a70f5b283d74003d3921254e34ff5d'
 )
 
