e94c29f077b153ed4e408037eb33bdc756effb38
Sky <befruitful12@gmail.com>
2022-03-02 20:53:35 -0500

    remove original

--- app/overrides/add_product_viewed_to_products_show.rb
+++ app/overrides/add_product_viewed_to_products_show.rb
@@ -2,15 +2,13 @@ Deface::Override.new(
     virtual_path: 'spree/products/show',
     name: 'add_product_viewed_to_products_show',
     insert_bottom: '[data-hook="product_show"]',
-    partial: 'spree/shared/trackers/segment/product_viewed.js',
-    :original => 'a34bc2cc550e15c59a19a85a9ff4c7f1b9713695'
+    partial: 'spree/shared/trackers/segment/product_viewed.js'
 )
 
 Deface::Override.new(
     virtual_path: 'spree/products/show',
     name: 'add_ga_product_viewed_to_products_show',
     insert_bottom: '[data-hook="product_show"]',
-    partial: 'spree/shared/trackers/google_analytics/product_viewed.js',
-    :original => 'a890bf88c4a70f5b283d74003d3921254e34ff5d'
+    partial: 'spree/shared/trackers/google_analytics/product_viewed.js'
 )
 
