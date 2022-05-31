96b69e4913df40471bd189922782727c3364a6df
QiuLin Wang <wangql.dev@gmail.com>
2022-05-30 23:43:37 -0400

    Added matomo site search tracking.

---
+++ app/overrides/add_matomo_search_to_products_list.rb
@@ -0,0 +1,9 @@
+spree_version = Gem.loaded_specs['spree_core'].version
+unless spree_version >= Gem::Version.create('3.4.0') && spree_version < Gem::Version.create('3.5.0')
+  Deface::Override.new(
+    virtual_path: 'spree/products/index',
+    name: 'add_products_searched_to_products_list',
+    insert_before: '[data-hook="search_results"]',
+    partial: 'spree/shared/trackers/matomo/products_searched.js',
+  )
+end
