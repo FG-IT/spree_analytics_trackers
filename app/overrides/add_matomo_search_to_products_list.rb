181205c7c46f5058448c0f891d66d407263c0cce
QiuLin Wang <wangql.dev@gmail.com>
2022-09-07 11:59:12 -0500

    Fixed matomo could not track site search error.

--- app/overrides/add_matomo_search_to_products_list.rb
+++ app/overrides/add_matomo_search_to_products_list.rb
@@ -2,7 +2,7 @@ spree_version = Gem.loaded_specs['spree_core'].version
 unless spree_version >= Gem::Version.create('3.4.0') && spree_version < Gem::Version.create('3.5.0')
   Deface::Override.new(
     virtual_path: 'spree/products/index',
-    name: 'add_products_searched_to_products_list',
+    name: 'add_products_searched_to_products_list_matomo',
     insert_before: '[data-hook="search_results"]',
     partial: 'spree/shared/trackers/matomo/products_searched.js',
   )
