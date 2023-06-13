4fa7e9500fb07ddd4f95e1598446799e4379c3d8
Neal Wong <neal.wkacc@gmail.com>
2023-06-13 13:37:30 -0500

    Added klaviyo add to cart.

--- app/assets/javascripts/spree/frontend/add_to_cart_analytics.js
+++ app/assets/javascripts/spree/frontend/add_to_cart_analytics.js
@@ -39,6 +39,45 @@ function gaAddToCart(variant, quantity, currency = 'USD') {
     );
 }
 
+function klaviyoAddToCart(product, variant, quantity, currency = 'USD') {
+    var price = typeof variant.price === 'object' ? variant.price.amount : variant.price
+    var product_url = location.href;
+    var categories = product.categories ? product.categories : [];
+    var image_url;
+    if (variant.images) {
+        image_url = variant.images[0].url_product;
+    } else {
+        if (product.images) {
+            image_url = product.images[0].url_product;
+        }
+    }
+    var cart = {
+        "value": price,
+        "AddedItemProductName": product.name,
+        "AddedItemProductID": variant.product_id,
+        "AddedItemSKU": variant.sku,
+        "AddedItemCategories": categories,
+        "AddedItemImageURL": image_url,
+        "AddedItemURL": product_url,
+        "AddedItemPrice": price,
+        "AddedItemQuantity": quantity,
+        "ItemNames": [product.name],
+        "CheckoutURL": location.href + '/checkout',
+        "Items": [{
+            "ProductID": variant.product_id,
+            "SKU": variant.sku,
+            "ProductName": product.name,
+            "Quantity": quantity,
+            "ItemPrice": price,
+            "RowTotal": price,
+            "ProductURL": product_url,
+            "ImageURL": image_url,
+            "ProductCategories": categories
+       }]
+    };
+    klaviyo.push(["track", "Added to Cart", cart]);
+}
+
 function segmentAddtoCart(variant, quantity, currency) {
     analytics.track('Product Added', {
         product_id: variant.id,
@@ -84,6 +123,7 @@ function matomoAddToCart(variant, quantity, currency, cart) {
 
 Spree.ready(function () {
     $('body').on('product_add_to_cart', function (event) {
+        var product = event.product;
         var variant = event.variant;
         var quantity = event.quantity_increment;
         var currency = event.cart.currency;
@@ -107,6 +147,10 @@ Spree.ready(function () {
 
         if (typeof obApi !== 'undefined') {
             obAddToCart(variant, quantity, currency)
+        }
+
+        if (typeof klaviyo !== 'undefined') {
+            klaviyoAddToCart(product, variant, quantity, currency)
         }
     })
 });
